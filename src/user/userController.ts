bd6dee733778303b5279db95ced3f86b7b6a7d85
DamodarPatil <patildamodar1983@gmail.com>
2024-08-27 22:00:12 +0530

    add bcrypt

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -16,6 +16,7 @@ const createUser = async (
     const error = createHttpError(400, "All Fields are required");
     return next(error);
   }
+
   // Database call
   const user = await User.findOne({ email }); // in javascript if the key and value of object are same you cam write like this.
 
@@ -23,6 +24,8 @@ const createUser = async (
     const error = createHttpError(400, "User already exists with this email");
     return next(error);
   }
+
+  // password -> hash 
 
   // Process
 
