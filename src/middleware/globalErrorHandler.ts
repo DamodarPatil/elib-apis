b7b570f0cbfd9dcd198b5cbfe347bc21ef7ff2b8
DamodarPatil <patildamodar1983@gmail.com>
2024-08-27 19:56:55 +0530

    Refactor environment configuration and add NODE_ENV variable

---
+++ src/middleware/globalErrorHandler.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import { HttpError } from "http-errors";
+import { config } from "../config/config";
+
+// Global error handler
+const globalErrorHandler = (
+  error: HttpError,
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  const statusCode = error.statusCode || 500;
+  return response.status(statusCode).json({
+    message: error.message || "Internal Server Error",
+    errorStack: config.env === "development" ? error.stack : "", // don't use in production
+  });
+};
+
+export default globalErrorHandler;
