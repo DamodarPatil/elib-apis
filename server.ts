0e9435f3e82f0411158ee1a631cc6cb6be31b644
DamodarPatil <patildamodar1983@gmail.com>
2024-08-27 17:00:09 +0530

    Refactor server.ts and app.ts files

--- server.ts
+++ server.ts
@@ -1,2 +1,11 @@
-console.log("Damodar");
- 
+import app from "./src/app";
+
+const startServer = () => {
+  const port = process.env.PORT || 3000;
+
+  app.listen(port, () => {
+    console.log(`Listening on port: ${port}`);
+  });
+};
+
+startServer();
--- src/app.ts
+++ src/app.ts
@@ -2,4 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (request, response, next) => {
+  response.json({
+    message: "Welcome",
+  });
+});
+
 export default app;
